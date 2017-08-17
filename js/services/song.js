df8ac126053d692373dcf173ea254be6b5a806b8
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-16 22:24:02 -0400

    built service

---
+++ js/services/song.js
@@ -0,0 +1,11 @@
+/* global angular */
+angular
+  .module('radio')
+  .factory('songs', [
+    '$resource',
+    songsService
+  ])
+
+function songsService ($resource) {
+  return $resource('http://localhost:3000/songs')
+}
